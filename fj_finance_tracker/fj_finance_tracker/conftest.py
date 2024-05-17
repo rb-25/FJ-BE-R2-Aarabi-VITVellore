679b0682ba1fe1eb5a52d8d41f773160b5e162d3
rb-25 <aarabibalakrishnan2@gmail.com>
2024-05-17 22:03:28 +0530

    feat: Cookiecutter initialisation

---
+++ fj_finance_tracker/fj_finance_tracker/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fj_finance_tracker.users.models import User
+from fj_finance_tracker.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
